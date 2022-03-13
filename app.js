f1b93b80aff2c910ce88f472affbaf5b8b199d0f
Habizanoor <habizaboor.cse@gmail.com>
2022-03-13 10:26:48 +0600

    more

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const images = [
-    './images/image11.jpg',
-    './images/image12.jpg',
-    './images/image13.jpg',
-    './images/image14.jpg'
+    './images/image1.jpg',
+    './images/image2.jpg',
+    './images/image3.jpg',
+    './images/image4.jpg',
+    './images/image5.jpg',
+    './images/image6.jpg',
+    './images/image7.jpg',
+    './images/image8.jpg'
 
 ];
 let imgIndex = 0;
